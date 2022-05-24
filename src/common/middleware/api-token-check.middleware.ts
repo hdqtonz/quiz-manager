d2404d72631ae60847b0c675c28dd32edca3cb9c
hdqtonz <hiten.qtonz@yahoo.com>
2022-05-24 10:21:38 +0530

    Update some files

--- src/common/middleware/api-token-check.middleware.ts
+++ src/common/middleware/api-token-check.middleware.ts
@@ -5,14 +5,14 @@ import {
   NestMiddleware,
 } from '@nestjs/common';
 import { NextFunction } from 'express';
-import { ApiTokenPayment } from './exceptions/api-token-payment.exception';
+import { ApiTokenPaymentException } from './exceptions/api-token-payment.exception';
 
 export class ApiTokenCheckMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (req.headers['api-token'] !== 'my-token') {
       // throw new BadRequestException('Token does not match');
       // throw new HttpException('My response', HttpStatus.PAYMENT_REQUIRED);
-      throw new ApiTokenPayment();
+      throw new ApiTokenPaymentException();
     }
     next();
   }
